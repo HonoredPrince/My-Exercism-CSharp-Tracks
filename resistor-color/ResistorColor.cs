3c1178cf3caf45cd1e6561dd8e55c40fe037f407
unknown <matheuspsnbrasil@hotmail.com>
2021-08-20 13:01:56 -0300

    Update finished exercises

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color) => color.Trim().ToLower() switch
+    private static Dictionary<string, int> resistorLabel = new Dictionary<string, int>()
     {
-        "black" => 0,
-        "brown" => 1,
-        "red" => 2,
-        "orange" => 3,
-        "yellow" => 4,
-        "green" => 5,
-        "blue" => 6,
-        "violet" => 7,
-        "grey" => 8,
-        "white" => 9,
-        _ => throw new ArgumentOutOfRangeException("Resistor color name is not valid"),
+        {"black", 0},
+        {"brown", 1},
+        {"red", 2},
+        {"orange", 3},
+        {"yellow", 4},
+        {"green", 5},
+        {"blue", 6},
+        {"violet", 7},
+        {"grey", 8},
+        {"white", 9}
     };
 
-    public static string[] Colors() => new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    public static int ColorCode(string color) => resistorLabel[color];
+
+    public static string[] Colors() => resistorLabel.Keys.ToArray();
 }
---
+++ space-age/SpaceAge.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class SpaceAge
+{
+    private double absoluteSecondsOfLife;
+    private double earthYearInSeconds = 31557600;
+
+    public SpaceAge(int seconds)
+    {
+        absoluteSecondsOfLife = seconds;
+    }
+
+    public double OnEarth() => absoluteSecondsOfLife / earthYearInSeconds;
+
+    public double OnMercury() => OnEarth() / 0.2408467;
+
+    public double OnVenus() => OnEarth() / 0.61519726;
+
+    public double OnMars() => OnEarth() / 1.8808158;
+
+    public double OnJupiter() => OnEarth() / 11.862615;
+
+    public double OnSaturn() => OnEarth() / 29.447498;
+
+    public double OnUranus() => OnEarth() / 84.016846;
+
+    public double OnNeptune() => OnEarth() / 164.79132;
+}
